c5e7a69b05d3080da9b23b4b3252613e38d482c7
Antonín Dufka <xdufka1@fi.muni.cz>
2023-05-10 13:29:25 +0200

    feat: include JCMathLib version in packaged file

--- package.py
+++ package.py
@@ -3,7 +3,7 @@ import re
 
 from argparse import ArgumentParser
 
-DIR = "./applet/src/main/java/opencrypto/jcmathlib/"
+DIR = "."
 
 CURVES = {"SecP256r1", "SecP256k1", "SecP512r1"}
 FILTERED_FILES = {"UnitTests.java", "Example.java", "Integer.java"}
@@ -53,6 +53,13 @@ def package_file(file, keep_locks=False):
     return os.linesep.join(lines)
 
 
+def get_version(file):
+    with open(file, "r") as f:
+        for line in f:
+            if (matches := re.search(r"version \'(.*)\'", line)):
+                return matches.group(1)
+    return "0.0"
+
 def main():
     parser = ArgumentParser(
         prog="package.py",
@@ -70,12 +77,16 @@ def main():
                         default="jcmathlib.java")
     args = parser.parse_args()
 
+    version = get_version(f"{args.dir}/applet/build.gradle")
+
+    sources = f"{args.dir}/applet/src/main/java/opencrypto/jcmathlib/"
+
     filtered_files = FILTERED_FILES.copy()
     if not args.keep_locks:
         filtered_files = filtered_files.union({"ObjectLocker.java"})
     included_files = sorted(map(
-        lambda x: args.dir + x, filter(lambda x: x.endswith(".java") and x not in filtered_files, os.listdir(args.dir))))
-    included_files += list(map(lambda x: f"{args.dir}curves/{x}.java", args.curves))
+        lambda x: sources + x, filter(lambda x: x.endswith(".java") and x not in filtered_files, os.listdir(sources))))
+    included_files += list(map(lambda x: f"{sources}curves/{x}.java", args.curves))
 
     imports = load_imports(included_files)
 
@@ -86,7 +97,7 @@ def main():
             print(imp, file=f)
         print(file=f)
         print("/**", file=f)
-        print(" * Packaged JCMathLib library (https://github.com/OpenCryptoProject/JCMathLib).", file=f)
+        print(f" * Packaged JCMathLib v{version} (https://github.com/OpenCryptoProject/JCMathLib).", file=f)
         print(" */", file=f)
         print("public class jcmathlib {", file=f)
 
